ad91a9f632c59592ffb2acc7f719ce9f8e68ed7d
munna-yadav <mail.munna50@gmail.com>
2025-08-18 00:16:32 +0530

    refactor: update kadanes algo for better understanding

--- array/BestTimeToBuyAndSell.java
+++ array/BestTimeToBuyAndSell.java
@@ -10,14 +10,16 @@ public class BestTimeToBuyAndSell {
         }
         return maxPro;
     }
+    
     static int optimal(int []arr){
-         int maxPro = 0;
-    int minPrice = Integer.MAX_VALUE;
-    for (int i = 0; i < arr.length; i++) {
-        minPrice = Math.min(minPrice, arr[i]);
-        maxPro = Math.max(maxPro, arr[i] - minPrice);
-    }
-    return maxPro;
+        int min = arr[0];
+        int maxPro = 0;
+        for (int i = 1; i < arr.length; i++){
+            int profit = arr[i] - min;
+            maxPro =  Integer.max(maxPro, profit);
+            min = Integer.min(min, arr[i]);
+        }
+        return maxPro;
     }
     public static void main(String[] args) {
          int arr[] = {7,1,5,3,6,4};

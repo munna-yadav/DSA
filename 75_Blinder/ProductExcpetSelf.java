c2e6b31d08560726555ca5bf15847cbaee9fdc3c
munna-yadav <mail.munna50@gmail.com>
2025-09-12 23:57:25 +0530

    Add optimal solution

--- 75_Blinder/ProductExcpetSelf.java
+++ 75_Blinder/ProductExcpetSelf.java
@@ -17,6 +17,39 @@ public class ProductExcpetSelf{
         return result;
     }
 
+    static int[] optimalSolution(int []nums){
+
+        int n = nums.length;
+
+        int []prefixProductFromStart = new int[n];
+        int []prefixProductFromEnd = new int[n];
+        int []result = new int[n];
+
+        //find product from left
+        prefixProductFromStart[0] = nums[0];
+        for(int i = 1; i < n; i++){
+            prefixProductFromStart[i] = prefixProductFromStart[i-1] * nums[i];
+        }
+
+        // storing product from right side of the array
+        prefixProductFromEnd[n-1] = nums[n -1];
+        for(int i = n-2; i > 0; i--){
+            prefixProductFromEnd[i] = prefixProductFromEnd[i + 1] * nums[i];
+        }
+
+
+        // storing the result
+
+        result[0] = prefixProductFromStart[0];
+        result[n -1 ] = prefixProductFromEnd[n -2];
+        for(int i = 1; i < n - 1; i++){
+            result[i] = prefixProductFromStart[ i - 1] * prefixProductFromEnd[ i + 1];
+        }
+
+        return result;
+
+    }
+
     public static void main(String[] args) {
         int []arr = {1,2,3,4};
         int []result = solution(arr);

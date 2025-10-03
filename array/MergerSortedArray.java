f17d3dd3926ed95e3ca07a8e7bb159ee5632c6aa
munna-yadav <mail.munna50@gmail.com>
2025-10-04 00:02:20 +0530

    Add optimal solution

--- array/MergerSortedArray.java
+++ array/MergerSortedArray.java
@@ -11,6 +11,26 @@ public class MergerSortedArray {
         }
         Arrays.sort(nums1);
     }
+
+    static void optimal(int []nums1, int n, int []nums2, int m){
+        int i = n -1;
+        int j = m -1;
+        int k = m + n - 1;
+
+        while( i >= 0 && j >= 0){
+
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            }
+            else{
+                nums2[k--] = nums2[j--];
+            }
+        }
+
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
     public static void main(String[] args) {
         int []nums1 = {1,2,3,0,0,0};
         int []nums2 = {2,5,6};

d92775df23d69310ad5ff147435b8e0547ec951f
munna-yadav <mail.munna50@gmail.com>
2025-10-05 23:39:57 +0530

    Remove duplate from array

--- array/RemoveDuplicate.java
+++ array/RemoveDuplicate.java
@@ -1,41 +1,38 @@
 import java.util.*;
 
 class RemoveDuplicate{
-    public static void main(String []args){
-        int []arr = {1,3,2,3,5,2};
-
+    static void solution(int []arr){
         Set<Integer> set = new HashSet<>();
 
-        for(int e: arr){
-            set.add(e);
+        for(int ele:arr){
+            set.add(ele);
         }
-
         int k = set.size();
-
         int j = 0;
 
-       for(int e:set){
-        arr[j++] = e;
-       }
 
-       for(int i = 0 ; i < k; i++){
-        System.out.print(arr[i]+" \t");
-       }
+        for(int e:set){
+            arr[j++] = e;
+        }
 
-       int []newArray = new int[k];
-       int i = 0;
+        for(int i = 0; i < k; i ++){
+            System.out.print(arr[i] +"\t");
+        }
+    }
+    public static void main(String []args){
+        int []arr = {1,3,2,3,5,2};
+        solution(arr);
 
+    //     Set<Integer> set = new HashSet<>();
 
-       // inserting and traversing using iterator
-       System.out.println();
-       Iterator<Integer> iterator = set.iterator();
-       while(iterator.hasNext()){
-        newArray[i] = iterator.next();
-        i++;
-       }
-       for(int e:newArray){
-        System.out.println(e);
-       }
+    //     for(int e: arr){
+    //         set.add(e);
+    //     }
+
+    //    Iterator<Integer> iterator = set.iterator();
+    //    while (iterator.hasNext()) {
+    //     System.out.println(iterator.next());
+    //    }
 
     }
 }
